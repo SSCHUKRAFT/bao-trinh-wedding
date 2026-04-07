1eecb0b893cf936bd2822888f2525001284e101b
Sean Schukraft <sschukra@oswego.edu>
2026-04-07 12:16:37 -0400

    Update gallery media and video behavior

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,63 +1,99 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './gallery.css';
 
-import { Box, IconButton, MobileStepper } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import { BiArrowToLeft, BiArrowToRight } from 'react-icons/bi';
 import SwipeableViews from 'react-swipeable-views';
 
-import Horizontal1 from '../../assets/home-photos/horizontal1.jpg';
-import Horizontal2 from '../../assets/home-photos/horizontal2.jpg';
-import Horizontal3 from '../../assets/home-photos/horizontal3.jpeg';
-import Horizontal4 from '../../assets/home-photos/horizontal4.jpeg';
-import Vertical1 from '../../assets/home-photos/vertical1.jpg';
-import Vertical2 from '../../assets/home-photos/vertical2.jpg';
-import Vertical3 from '../../assets/home-photos/vertical3.jpeg';
-import Vertical4 from '../../assets/home-photos/vertical4.jpeg';
-import Vertical5 from '../../assets/home-photos/vertical5.jpeg';
-import Vertical6 from '../../assets/home-photos/vertical6.jpeg';
-import Vertical7 from '../../assets/home-photos/vertical7.jpeg';
-import Vertical8 from '../../assets/home-photos/vertical8.jpeg';
-import Vertical9 from '../../assets/home-photos/vertical9.jpeg';
-import Vertical10 from '../../assets/home-photos/vertical10.jpeg';
-import Vertical11 from '../../assets/home-photos/vertical11.jpeg';
-import Vertical12 from '../../assets/home-photos/vertical12.jpeg';
-import Vertical13 from '../../assets/home-photos/vertical13.jpeg';
-import Vertical14 from '../../assets/home-photos/vertical14.jpeg';
-import Vertical15 from '../../assets/home-photos/vertical15.jpeg';
-import Vertical16 from '../../assets/home-photos/vertical16.jpeg';
-import Vertical17 from '../../assets/home-photos/vertical17.jpeg';
-import Vertical18 from '../../assets/home-photos/vertical18.jpeg';
-import Vertical19 from '../../assets/home-photos/vertical19.jpeg';
+const mediaContext = require.context('../../assets/home-photos', false, /\.(jpe?g|png|mp4)$/i);
 
-const images = [
-  { src: Horizontal1, alt: 'Gallery photo 1' },
-  { src: Horizontal2, alt: 'Gallery photo 2' },
-  { src: Vertical1, alt: 'Gallery photo 3' },
-  { src: Vertical2, alt: 'Gallery photo 4' },
-  { src: Vertical3, alt: 'Gallery photo 5' },
-  { src: Vertical4, alt: 'Gallery photo 6' },
-  { src: Vertical5, alt: 'Gallery photo 7' },
-  { src: Vertical6, alt: 'Gallery photo 8' },
-  { src: Vertical7, alt: 'Gallery photo 9' },
-  { src: Vertical8, alt: 'Gallery photo 10' },
-  { src: Vertical9, alt: 'Gallery photo 11' },
-  { src: Vertical10, alt: 'Gallery photo 12' },
-  { src: Vertical11, alt: 'Gallery photo 13' },
-  { src: Vertical12, alt: 'Gallery photo 14' },
-  { src: Vertical13, alt: 'Gallery photo 15' },
-  { src: Vertical14, alt: 'Gallery photo 16' },
-  { src: Vertical15, alt: 'Gallery photo 17' },
-  { src: Vertical16, alt: 'Gallery photo 18' },
-  { src: Vertical17, alt: 'Gallery photo 19' },
-  { src: Vertical18, alt: 'Gallery photo 20' },
-  { src: Vertical19, alt: 'Gallery photo 21' },
-  { src: Horizontal3, alt: 'Gallery photo 22' },
-  { src: Horizontal4, alt: 'Gallery photo 23' },
-];
+const mediaTypePriority = {
+  image: 0,
+  video: 1,
+};
+
+const mediaGroupPriority = {
+  horizontal: 0,
+  vertical: 1,
+  vud: 2,
+  other: 3,
+};
+
+const getMediaType = (filename) => {
+  if (/\.mp4$/i.test(filename)) {
+    return 'video';
+  }
+
+  return 'image';
+};
+
+const getMediaGroup = (filename) => {
+  const normalizedName = filename.toLowerCase();
+
+  if (normalizedName.startsWith('horizontal')) {
+    return 'horizontal';
+  }
+
+  if (normalizedName.startsWith('vertical')) {
+    return 'vertical';
+  }
+
+  if (normalizedName.startsWith('vud')) {
+    return 'vud';
+  }
+
+  return 'other';
+};
+
+const mediaItems = mediaContext
+  .keys()
+  .map((key) => {
+    const filename = key.replace('./', '');
+    const type = getMediaType(filename);
+
+    return {
+      src: mediaContext(key),
+      alt: filename.replace(/\.[^.]+$/, ''),
+      type,
+      filename,
+      group: getMediaGroup(filename),
+    };
+  })
+  .sort((firstItem, secondItem) => {
+    const typeDifference =
+      mediaTypePriority[firstItem.type] - mediaTypePriority[secondItem.type];
+
+    if (typeDifference !== 0) {
+      return typeDifference;
+    }
+
+    const groupDifference =
+      mediaGroupPriority[firstItem.group] - mediaGroupPriority[secondItem.group];
+
+    if (groupDifference !== 0) {
+      return groupDifference;
+    }
+
+    return firstItem.filename.localeCompare(secondItem.filename, undefined, {
+      numeric: true,
+      sensitivity: 'base',
+    });
+  });
 
 const Gallery = () => {
   const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = images.length;
+  const videoRefs = useRef({});
+  const maxSteps = mediaItems.length;
+
+  useEffect(() => {
+    Object.entries(videoRefs.current).forEach(([index, videoElement]) => {
+      if (!videoElement || Number(index) === activeStep) {
+        return;
+      }
+
+      videoElement.pause();
+    });
+  }, [activeStep]);
 
   const handleNext = () => {
     if (activeStep < maxSteps - 1) {
@@ -85,20 +121,40 @@ const Gallery = () => {
           resistance
           className="gallery-swipe-wrapper"
         >
-          {images.map((image, index) => (
-            <div className="gallery-slide" key={index}>
-              <div
-                className="gallery-blurred-bg"
-                style={{ backgroundImage: `url(${image.src})` }}
-              />
+          {mediaItems.map((mediaItem, index) => (
+            <div className="gallery-slide" key={`${mediaItem.filename}-${index}`}>
+              {mediaItem.type === 'image' && (
+                <div
+                  className="gallery-blurred-bg"
+                  style={{ backgroundImage: `url(${mediaItem.src})` }}
+                />
+              )}
               <div className="gallery-bg-overlay" />
 
-              <img
-                className="gallery-image"
-                src={image.src}
-                alt={image.alt}
-                loading="lazy"
-              />
+              {mediaItem.type === 'video' ? (
+                <video
+                  className="gallery-media gallery-video"
+                  src={mediaItem.src}
+                  aria-label={mediaItem.alt}
+                  controls
+                  playsInline
+                  preload="metadata"
+                  ref={(element) => {
+                    if (element) {
+                      videoRefs.current[index] = element;
+                    } else {
+                      delete videoRefs.current[index];
+                    }
+                  }}
+                />
+              ) : (
+                <img
+                  className="gallery-media gallery-image"
+                  src={mediaItem.src}
+                  alt={mediaItem.alt}
+                  loading="lazy"
+                />
+              )}
             </div>
           ))}
         </SwipeableViews>
@@ -118,17 +174,6 @@ const Gallery = () => {
         >
           <BiArrowToRight />
         </IconButton>
-
-        <div className="gallery-controls-overlay">
-          <MobileStepper
-            steps={maxSteps}
-            position="static"
-            activeStep={activeStep}
-            nextButton={null}
-            backButton={null}
-            className="gallery-stepper"
-          />
-        </div>
       </Box>
     </div>
   );
